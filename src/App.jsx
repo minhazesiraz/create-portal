7af3240ba576f11e661f7b73ddd8d977d35e7ec0
Minhaz E Siraz <minhazesiraz@gmail.com>
2025-02-24 01:24:18 +0600

    createPortal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TraditionalModal from "./traditional-modal/TraditionalModal"
+
+import PortalModal from "./modal/portal-modal/PortalModal"
+
+function App() {
+
+  return (
+    <>
+      <TraditionalModal />
+      <PortalModal />
+    </>
+  )
+}
+
+export default App
